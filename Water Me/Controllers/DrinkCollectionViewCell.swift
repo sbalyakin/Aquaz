3128e771ca2fd5f3ec05ae5486b621193e9b9821
Sergey Balyakin <sbalyakin@gmail.com>
2014-12-29 19:13:34 +0300

    Task #0001, Select drink page has been re-implemented. From now it's based on collection view.

---
+++ Water Me/Controllers/DrinkCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DrinkCollectionViewCell.swift
+//  Water Me
+//
+//  Created by Sergey Balyakin on 29.12.14.
+//  Copyright (c) 2014 Sergey Balyakin. All rights reserved.
+//
+
+import UIKit
+
+class DrinkCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+    
+}
--- Water Me/Controllers/SelectDrinkViewController.swift
+++ Water Me/Controllers/SelectDrinkViewController.swift
@@ -2,27 +2,69 @@
 //  SelectDrinkViewController.swift
 //  Water Me
 //
-//  Created by Sergey Balyakin on 03.10.14.
+//  Created by Sergey Balyakin on 29.12.14.
 //  Copyright (c) 2014 Sergey Balyakin. All rights reserved.
 //
 
 import UIKit
 
-class SelectDrinkViewController: UIViewController {
+class SelectDrinkViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  @IBOutlet weak var collectionView: UICollectionView!
   
   var dayViewController: DayViewController!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    rowsCount = Int(ceil(Float(Drink.getDrinksCount()) / Float(columnsCount)))
   }
   
-  @IBAction func drinkTapped(sender: AnyObject) {
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return Drink.getDrinksCount()
+  }
+  
+  // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("DrinkCell", forIndexPath: indexPath) as DrinkCollectionViewCell
+    
+    let drinkIndex = indexPath.row
+    assert(drinkIndex < Drink.getDrinksCount())
+    
+    if let drink = Drink.getDrinkByIndex(drinkIndex) {
+      cell.titleLabel.text = drink.localizedName
+    } else {
+      assert(false)
+    }
+    
+    return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    let layout = collectionViewLayout as UICollectionViewFlowLayout
+    let contentWidth = collectionView.frame.width - layout.minimumInteritemSpacing * CGFloat(columnsCount - 1)
+    let contentHeight = collectionView.frame.height - layout.minimumLineSpacing * CGFloat(rowsCount - 1)
+    let cellWidth = trunc(contentWidth / CGFloat(columnsCount))
+    let cellHeight = trunc(contentHeight / CGFloat(rowsCount))
+    let size = CGSizeMake(cellWidth, cellHeight)
+    return size
+  }
+  
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    let drinkIndex = indexPath.row
+    assert(drinkIndex < Drink.getDrinksCount())
+    let drink = Drink.getDrinkByIndex(drinkIndex)
+    
     let consumptionViewController = storyboard!.instantiateViewControllerWithIdentifier("ConsumptionViewController") as ConsumptionViewController
-    let drink = Drink.getDrinkByIndex(sender.tag)
     consumptionViewController.drink = drink
     consumptionViewController.currentDate = dayViewController.getCurrentDate()
     consumptionViewController.dayViewController = dayViewController
     navigationController!.pushViewController(consumptionViewController, animated: true)
   }
   
+  let columnsCount = 3
+  var rowsCount = 0
 }
