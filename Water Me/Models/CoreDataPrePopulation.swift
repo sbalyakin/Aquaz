9cd7672440d8d73fd182736be61a43819c250551
Sergey Balyakin <sbalyakin@gmail.com>
2015-01-19 19:52:00 +0300

    Task #0001, Water percentage in drinks has been adjusted.

--- Water Me/Models/CoreDataPrePopulation.swift
+++ Water Me/Models/CoreDataPrePopulation.swift
@@ -25,15 +25,15 @@ class CoreDataPrePopulation {
         // Add drinks
         Drink.addEntity(index: Drink.DrinkType.Water.rawValue,   name: "Water",   waterPercent: 1.00, recentAmount: 250, managedObjectContext: managedObjectContext)
         Drink.addEntity(index: Drink.DrinkType.Coffee.rawValue,  name: "Coffee",  waterPercent: 0.98, recentAmount: 250, managedObjectContext: managedObjectContext)
-        Drink.addEntity(index: Drink.DrinkType.Tea.rawValue,     name: "Tea",     waterPercent: 0.98, recentAmount: 250, managedObjectContext: managedObjectContext)
+        Drink.addEntity(index: Drink.DrinkType.Tea.rawValue,     name: "Tea",     waterPercent: 0.99, recentAmount: 250, managedObjectContext: managedObjectContext)
 
-        Drink.addEntity(index: Drink.DrinkType.Soda.rawValue,    name: "Soda",    waterPercent: 0.98, recentAmount: 250, managedObjectContext: managedObjectContext)
+        Drink.addEntity(index: Drink.DrinkType.Soda.rawValue,    name: "Soda",    waterPercent: 0.89, recentAmount: 250, managedObjectContext: managedObjectContext)
         Drink.addEntity(index: Drink.DrinkType.Juice.rawValue,   name: "Juice",   waterPercent: 0.85, recentAmount: 250, managedObjectContext: managedObjectContext)
-        Drink.addEntity(index: Drink.DrinkType.Milk.rawValue,    name: "Milk",    waterPercent: 0.80, recentAmount: 250, managedObjectContext: managedObjectContext)
+        Drink.addEntity(index: Drink.DrinkType.Milk.rawValue,    name: "Milk",    waterPercent: 0.87, recentAmount: 250, managedObjectContext: managedObjectContext)
 
         Drink.addEntity(index: Drink.DrinkType.Alcohol.rawValue, name: "Alcohol", waterPercent: 0.30, recentAmount: 250, managedObjectContext: managedObjectContext)
         Drink.addEntity(index: Drink.DrinkType.Sport.rawValue,   name: "Sport",   waterPercent: 1.10, recentAmount: 250, managedObjectContext: managedObjectContext)
-        Drink.addEntity(index: Drink.DrinkType.Energy.rawValue,  name: "Energy",  waterPercent: 0.95, recentAmount: 250, managedObjectContext: managedObjectContext)
+        Drink.addEntity(index: Drink.DrinkType.Energy.rawValue,  name: "Energy",  waterPercent: 0.9, recentAmount: 250, managedObjectContext: managedObjectContext)
         
         // Add preliminary consumption rate for today
         ConsumptionRate.addEntity(
