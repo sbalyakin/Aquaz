ea664280c7a046447702caf17893b73a955771c7
Sergey Balyakin <sbalyakin@gmail.com>
2016-05-17 15:21:16 +0300

    Task #108, Build number has been updated to 70.

--- Pods/Target Support Files/Pods-Aquaz-MMWormhole/Pods-Aquaz-MMWormhole-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Aquaz_MMWormhole : NSObject
-@end
-@implementation PodsDummy_Pods_Aquaz_MMWormhole
-@end
--- Pods/Target Support Files/Pods-Widget-MMWormhole/Pods-Widget-MMWormhole-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Widget_MMWormhole : NSObject
-@end
-@implementation PodsDummy_Pods_Widget_MMWormhole
-@end
