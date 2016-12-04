2b948452f5334a4e707ed6f9cdda0ad50a0f86ec
Sergey Balyakin <sbalyakin@gmail.com>
2016-12-04 20:21:48 +0300

    Task #235, Statistics page has been modified in order to support full version checks for AquazLite.

--- Aquaz/Controllers/StatisticsViewController.swift
+++ Aquaz/Controllers/StatisticsViewController.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
-class StatisticsViewController: UIViewController {
+final class StatisticsViewController: UIViewController {
   
   @IBOutlet weak var segmentedControl: UISegmentedControl!
 
   weak var pageViewController: StatisticsPageViewController!
   
+  #if AQUAZLITE
+  private struct LocalizedStrings {
+    lazy var fullVersionBannerText = NSLocalizedString("SVC:Statistics are available in the full version only.",
+      value: "Statistics are available in the full version only.",
+      comment: "StatisticsViewController: Text for banner shown to promote the full version of Aquaz")
+    
+    lazy var demoOverlayText = NSLocalizedString("SVC:DEMO",
+      value: "DEMO",
+      comment: "StatisticsViewController: Caption above statistics page shown for not-full version of Aquaz. It should be uppercased.")
+  }
+  
+  private var fullVersionBannerView: InfoBannerView?
+  private var demoOverlayView: UIView?
+  private var localizedStrings = LocalizedStrings()
+  #endif
+  
   fileprivate struct Constants {
     static let pageViewControllerEmbeddingSegue = "Page View Controller Embedding"
+    #if AQUAZLITE
+    static let fullVersionViewControllerIdentifier = "FullVersionViewController"
+    #endif
   }
 
   override func viewDidLoad() {
@@ -26,11 +45,22 @@ class StatisticsViewController: UIViewController {
     segmentedControl.backgroundColor = StyleKit.controlTintColor
     
     initStatisticsPage()
+    
+    #if AQUAZLITE
+    checkFullVersion()
+    
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(fullVersionIsPurchased(_:)),
+                                           name: NSNotification.Name(rawValue: GlobalConstants.notificationFullVersionIsPurchased),
+                                           object: nil)
+    #endif
   }
 
+  #if AQUAZLITE
   deinit {
     NotificationCenter.default.removeObserver(self)
   }
+  #endif
   
   fileprivate func applyStyle() {
     UIHelper.applyStyleToViewController(self)
@@ -68,5 +98,83 @@ class StatisticsViewController: UIViewController {
       assert(false, "Unknown statistics page is activated")
     }
   }
+  
+  #if AQUAZLITE
+  private func checkFullVersion() {
+    if !Settings.sharedInstance.generalFullVersion.value {
+      showDemoOverlay()
+      showFullVersionBanner()
+    }
+  }
+  
+  private func showFullVersionBanner() {
+    assert(fullVersionBannerView == nil)
+    
+    fullVersionBannerView = InfoBannerView.create()
+    fullVersionBannerView!.infoLabel.text = localizedStrings.fullVersionBannerText
+    fullVersionBannerView!.infoImageView.image = ImageHelper.loadImage(.BannerFullVersion)
+    fullVersionBannerView!.bannerWasTappedFunction = { [weak self] _ in self?.fullVersionBannerWasTapped() }
+    fullVersionBannerView!.showDelay = 0.6
+    fullVersionBannerView!.show(animated: true, parentView: view)
+  }
+  
+  func fullVersionIsPurchased(_ notification: NSNotification) {
+    hideDemoOverlay()
+    hideFullVersionBanner()
+  }
+  
+  private func hideFullVersionBanner() {
+    fullVersionBannerView?.hide(animated: false) { _ in
+      self.fullVersionBannerView = nil
+    }
+  }
+  
+  func fullVersionBannerWasTapped() {
+    if let fullVersionViewController: FullVersionViewController = LoggedActions.instantiateViewController(storyboard: storyboard, storyboardID: Constants.fullVersionViewControllerIdentifier) {
+      navigationController!.pushViewController(fullVersionViewController, animated: true)
+    }
+  }
+  
+  private func showDemoOverlay() {
+    assert(demoOverlayView == nil)
+    
+    demoOverlayView = UIView()
+    demoOverlayView!.translatesAutoresizingMaskIntoConstraints = false
+    demoOverlayView!.backgroundColor = view.backgroundColor!.colorWithAlpha(0.7)
+    view.addSubview(demoOverlayView!)
+    
+    let demoLabel = UILabel()
+    demoLabel.translatesAutoresizingMaskIntoConstraints = false
+    demoLabel.textColor = UIColor.white
+    demoLabel.backgroundColor = UIColor.clear
+    demoLabel.alpha = 0.85
+    demoLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 100)
+    demoLabel.adjustsFontSizeToFitWidth = true
+    demoLabel.textAlignment = .center
+    demoLabel.numberOfLines = 1
+    demoLabel.text = localizedStrings.demoOverlayText
+    
+    demoOverlayView!.addSubview(demoLabel)
+    
+    // Setup constraints
+    
+    let overlayViews = ["overlay": demoOverlayView!]
+    view.addConstraints("H:|-0-[overlay]", views: overlayViews)
+    view.addConstraints("H:[overlay]-0-|", views: overlayViews)
+    view.addConstraints("V:|-0-[overlay]", views: overlayViews)
+    view.addConstraints("V:[overlay]-0-|", views: overlayViews)
+    
+    let labelViews = ["label": demoLabel]
+    demoOverlayView!.addConstraints("H:|-30-[label]", views: labelViews)
+    demoOverlayView!.addConstraints("H:[label]-30-|", views: labelViews)
+    demoOverlayView!.addConstraints("V:|-30-[label]", views: labelViews)
+    demoOverlayView!.addConstraints("V:[label]-30-|", views: labelViews)
+  }
+  
+  private func hideDemoOverlay() {
+    demoOverlayView?.removeFromSuperview()
+    demoOverlayView = nil
+  }
+  #endif
   
 }
