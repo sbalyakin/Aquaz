4c2e2253a0e81d350802060e0c2d6ebb99fead61
Sergey Balyakin <sbalyakin@gmail.com>
2016-12-20 13:08:10 +0300

    Task #108, Some nice colorization has been applied to the script.

--- Distribution/UpdateRepository.sh
+++ Distribution/UpdateRepository.sh
@@ -9,25 +9,31 @@
 ###########################################################################################
 # IMPORTANT: read readme.txt file before usage
 
+COLOR_RED='\033[0;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_BLUE='\033[0;36m'
+COLOR_NONE='\033[0m'
+
 AQUAZ_INFOPLIST_FILE=../Aquaz/Info.plist
 buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$AQUAZ_INFOPLIST_FILE")
 
 # Commit updated Info.plist
-echo "### Commit the updated Info.plist files."
+echo "${COLOR_BLUE}Commit the updated Info.plist files.${COLOR_NONE}"
 git commit -a -m "Task #108, Build number has been updated to $buildNumber."
 
 # Push the changes
-echo "### Push the changes to the repository."
+echo "\n${COLOR_BLUE}Push the changes to the repository.${COLOR_NONE}"
 git push --all
 
 # Add tag
 shortVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$AQUAZ_INFOPLIST_FILE")
 fullVersion=$shortVersion.$buildNumber
-echo "### Add tag for version $fullVersion."
+echo "\n${COLOR_BLUE}Add tag for version $fullVersion.${COLOR_NONE}"
 git tag -a v$fullVersion -m "Version $fullVersion"
 
 # Push the tag
-echo "### Push the new tag to the repository."
+echo "\n${COLOR_BLUE}Push the new tag to the repository.${COLOR_NONE}"
 git push origin v$fullVersion
 
-echo "### Done."
+echo "\n${COLOR_GREEN}Done.${COLOR_NONE}"
